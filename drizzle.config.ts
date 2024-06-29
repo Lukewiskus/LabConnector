b69e6378ad48e7009e97e39523f766f514f4620c
Luke Wiskus <lukewiskus@gmail.com>
2024-06-29 01:28:34 -0500

    Luke Wiskus: different env different db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,16 @@
 // drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
 
-import dotenv from "dotenv";
-import { defineConfig } from "drizzle-kit"
-
-
-dotenv.config({ path: ".env.local" });
-
-export default defineConfig({
+const config = defineConfig({
     dialect: "postgresql", // "postgresql" | "mysql"
     schema: "./src/drizzle/schema.ts",
     dbCredentials: {
-        database: process.env.POSTGRES_DATABASE ?? "",
-        url: process.env.DATABASE_URL ?? "",
-        user: process.env.POSTGRES_USERNAME ?? "",
-        password: process.env.POSTGRES_PASSWORD ?? "",
+        database: process.env.NODE_ENV === 'production' ? process.env.POSTGRES_DATABASE ?? "" : process.env.POSTGRES_DATABASE_DEV ?? "",
+        url: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL ?? "" : process.env.DATABASE_URL_DEV ?? "",
+        user: process.env.NODE_ENV === 'production' ? process.env.POSTGRES_USERNAME ?? "" : process.env.POSTGRES_USERNAME_DEV ?? "",
+        password: process.env.NODE_ENV === 'production' ? process.env.POSTGRES_PASSWORD ?? "" : process.env.POSTGRES_PASSWORD_DEV ?? "",
     },
-})
+});
+
+console.log(config)
+export default config;
--- src/drizzle/config.ts
+++ src/drizzle/config.ts
@@ -4,7 +4,7 @@ import postgres from "postgres";
 
 import * as schema from "@/drizzle/schema";
 
-const connectionString = process.env.DATABASE_URL || "";
+const connectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL ?? "" : process.env.DATABASE_URL_DEV ?? ""
 console.log(connectionString)
 const client = postgres(connectionString);
 const db = drizzle(client, { schema });
